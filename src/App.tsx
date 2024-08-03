f9ad790562ef1cca97cd5fa31cb2787fd4403b49
Hossaini <1989hossaini1@gmail.com>
2024-08-03 11:13:03 +0200

    template ts mit fetch data

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ComponentName from './components/ComponentName'
+
+function App() {
+ 
+
+  return (
+    <>App
+<ComponentName />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ComponentName.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react"
+
+// Nokte agar sakhtare object gefetch shode ma fargh darad bayad ma ham sakhtare DataTaype ra taghir dahim !
+
+type DataType = {
+    id: number
+    title: string
+}
+
+const ComponentName: React.FC = () => {
+
+    const [todos, setTodos] = useState<DataType[]>([])
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState<string | null>()
+
+    const url: string = 'https://jsonplaceholder.typicode.com/todos'
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+
+            try {
+                const res = await fetch(url)
+                if (!res.ok) {
+                    throw new Error('Faild to fetch user...')
+                }
+
+                const resData: DataType[] = await res.json()
+                setTodos(resData)
+
+            } catch (error) {
+                console.log(error);
+                setIsError((error as Error).message)
+
+            } finally {
+                setIsLoading(false)
+            }
+
+        }
+
+        fetchData()
+    }, [])
+
+    if (isLoading) return <h2>Loading...</h2>
+    if (isError) return <h2>Error: {isError}</h2>
+
+    return (
+        <div>
+            {todos.map(todo => {
+                return (
+                    <p key={todo.id}>title:{todo.title}</p>
+                )
+            })}
+
+        </div>
+    )
+}
+export default ComponentName
